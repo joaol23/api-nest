b5aa172fc711d507e81ee3eb51563ce7faf4f77c
joaol23 <joaolucas.m.ferreira2008@gmail.com>
2023-11-28 12:38:20 -0300

    test(e2e): fazendo teste end to end de teste

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { INestApplication } from "@nestjs/common";
 import * as request from "supertest";
 import { AppModule } from "./../src/app.module";
+import { AuthRegisterDTO } from "@/modules/auth/DTO/auth-register.dto";
 
 describe("AppController (e2e)", () => {
     let app: INestApplication;
@@ -20,6 +21,20 @@ describe("AppController (e2e)", () => {
             .get("/")
             .expect(200)
             .expect("Hello World!");
+    });
+
+    it("Registrar um novo usuario", async () => {
+        const authRegisterDTO: AuthRegisterDTO = {
+            name: "teste",
+            email: "teste@example.com",
+            password: "123456",
+        };
+        const response = await request(app.getHttpServer())
+            .post("/auth/register")
+            .send(authRegisterDTO)
+            .expect(201);
+
+        expect(typeof response.body.access_token).toEqual("string");
     });
 
     afterEach(() => {
